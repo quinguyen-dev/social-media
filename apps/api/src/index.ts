3c1b0802b5c5f8c075efe7816d49bec969fd8e66
Qui Nguyen <quinguyen.dev@gmail.com>
2023-10-16 20:06:15 -0400

    Fixed bug with apps/api/.tsconfig regarding 'tsconfig/base.json'. Seemed to be a bun-related issue.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { createServer } from "./server";
+
+const port = process.env.PORT || 5001;
+const server = createServer();
+
+server.listen(port, () => {
+  console.log("listening on port " + port);
+});
