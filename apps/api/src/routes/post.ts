4f630507afa2506b67eb2251f7ecdf5270ae538c
Qui Nguyen <quinguyen.dev@gmail.com>
2023-10-19 15:43:21 -0400

    Created a POST for a new post. Does not currently include req.params to fufill the request.

--- apps/api/src/routes/post.ts
+++ apps/api/src/routes/post.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response, NextFunction } from "express";
+import express, { Router, Request, Response } from "express";
 import { prismaClient } from "../prisma.client";
 
 const postRouter: Router = express.Router();
@@ -13,8 +13,27 @@ postRouter.get("/", async function (_, res: Response) {
   }
 });
 
-/* Get all the post of a specific user */
+/* Create a new post */
+postRouter.post("/", async function (req: Request, res: Response) {
+  // todo: check authentication
+  try {
+    const resource = await prismaClient.post.create({
+      data: {
+        authorId: 1,
+        content: "This is a new post.",
+      },
+    });
+
+    res.header(`/posts/${resource.postId}`).status(201).send(resource);
+  } catch (err) {
+    console.error("This request is not valid!!");
+    res.sendStatus(409);
+  }
+});
+
+/* Get all the post of a specific uers (potentially move to User router) */
 postRouter.get("/:userId", async function (req: Request, res: Response) {
+  // todo: potentially validate this before attempting to query
   try {
     const posts = await prismaClient.post.findMany({
       where: {
